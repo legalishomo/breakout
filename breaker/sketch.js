26010763519af121225dcc2f0f426d5673de9b1e
Omar Torres <01omartorres@gmail.com>
2017-11-12 10:27:26 -0500

    modify paddle class

--- breaker/sketch.js
+++ breaker/sketch.js
@@ -1,28 +1,37 @@
 
 // ==========PADDLE SETUP
-var speed = 8
-var paddle_x = 350;
-var paddle_width = 60
+var speed = 10
+var paddle_full_width = 200
+var paddle_edges_width = 25
+var paddle_center_areas_width= 50
+// 20 - 50 - 50 - 50 - 20
+var paddle_x = 350 - (paddle_center_areas_width/2)
 const paddle_height = 20
 
-function Paddle(x,y){
+
+function Paddle(x,y,width){
   this.x = x
   this.y = y
-  this.width = paddle_width,
+  this.width = width
   this.height = paddle_height
 }
 
 Paddle.prototype.display = function(){
-  noStroke()
+  stroke("red")
   fill("white")
   // rectMode(RADIUS) sets the x, y of rect() to now be the center point of the rectangle
   // and the w,h to be half of the rect()'s width and height
-  rect(this.x, this.y, this.width, this.height)
+  rect(this.x-paddle_center_areas_width-paddle_edges_width, this.y, paddle_edges_width, this.height)
+  rect(this.x-paddle_center_areas_width, this.y, paddle_center_areas_width, this.height)
+  rect(this.x, this.y, paddle_center_areas_width, this.height)
+  rect(this.x+paddle_center_areas_width, this.y, paddle_center_areas_width,this.height)
+  rect(this.x+(paddle_center_areas_width*2), this.y, paddle_edges_width, this.height)
 }
 
 Paddle.prototype.update = function(){
   this.x = paddle_x
 }
+
 
 // ========BALL setup
 
@@ -69,11 +78,9 @@ Ball.prototype.changeBallAngle = function(angle){
   let new_change_y = sin(angle) * this.delta_distance
   let new_change_x = cos(angle) * this.delta_distance
   if(Math.sign(this.change_y) == -1){
-    debugger
     this.change_y = -new_change_y
     this.change_x = new_change_x
   } else{
-    debugger
     this.change_y = new_change_y
     this.change_x = new_change_x
   }
@@ -93,7 +100,7 @@ BallToPaddleLine.prototype.render = function(){
   line(this.x1, this.y1, this.x2, this.y2)
 }
 
-BallToPaddleLine.prototype.update = function(paddle, paddle_width, ball_x_pos, ball_y_pos){
+BallToPaddleLine.prototype.update = function(paddle, paddle_width, ball_x_pos, ball_y_pos, edge){
   this.x1 = ball_x_pos
   this.y1 = ball_y_pos
   if(ball_x_pos<=paddle.x){
@@ -103,11 +110,12 @@ BallToPaddleLine.prototype.update = function(paddle, paddle_width, ball_x_pos, b
   }else{
     this.x2 = ball_x_pos
   }
-
-  if(ball_y_pos>=paddle.y){
-    this.y2 = ball_y_pos
-  }else{
-    this.y2 = paddle.y
+  if(edge){
+    if(ball_y_pos>=paddle.y){
+      this.y2 = ball_y_pos
+    }else{
+      this.y2 = paddle.y
+    }
   }
 }
 
@@ -129,16 +137,16 @@ function ballCollideWithWall(circle_axis, circle_radius, canvas_dimension){
 
 // ==============SKETCH
 
-var start_positon = true
+// var start_positon = true
 
 function setup() {
   // createCanvas(w,h)
   // 'height' is variable in P5 that is set to the canvas' height
   angleMode(DEGREES)
   createCanvas(700, 700)
-  paddle = new Paddle(paddle_x, height-40)
+  paddle = new Paddle(paddle_x, height-40, paddle_full_width)
   ball = new Ball(ball_start_x, ball_start_y, ball_radius)
-  // paddle_line = new BallToPaddleLine(mouseX, mouseY, paddle_x, height-40)
+  ball_to_center_paddle_line = new BallToPaddleLine(ball_start_x, ball_start_y, paddle_x, height-40)
 }
 
 // draw() continuously executes the code in the block until program stops
@@ -148,18 +156,16 @@ function draw() {
   // ellipse(x,y,w,[h])
   // background(R, G, B)
   background(105,105,105)
-  if(start_positon){
 
-  }
   paddle.update()
   ball.update()
-  // paddle_line.update(paddle, paddle_width, mouseX, mouseY)
+  ball_to_center_paddle_line.update(paddle, paddle_center_areas_width, ball.x, ball.y)
   paddle.display()
   ball.display()
-  // paddle_line.render()
+  ball_to_center_paddle_line.render()
 
   if(keyIsDown(LEFT_ARROW)){
-    if(paddle_x == 0){
+    if(paddle_x <= 0){
       paddle_x
     } else {
       paddle_x -= speed
@@ -167,7 +173,7 @@ function draw() {
   }
 
   if(keyIsDown(RIGHT_ARROW)){
-    if(paddle_x == 500){
+    if(paddle_x == width - paddle_full_width){
       paddle_x
     }else{
       paddle_x += speed
@@ -184,9 +190,18 @@ function draw() {
   if(ballCollideWithWall(ball.y, ball.radius, height)){
     console.log("hit f or c")
     ball.changeBallYDirection()
+    // ball.changeBallAngle(15)
+  }
+
+  if(ballCollideWithPaddle(ball.x, ball.y, ball.radius, ball_to_center_paddle_line.x2, ball_to_center_paddle_line.y2)){
+    console.log("hitting paddle")
+    ball.changeBallYDirection()
     ball.changeBallAngle(15)
   }
 
+  // if(ballCollideWithPaddle(ball.x, ball.y, ball.radius, point_x, point_y))
+  //
+  //
 
 }
 
