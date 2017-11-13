240c4d63a3827624e13af3fddef3ec2a63de841d
Omar Torres <01omartorres@gmail.com>
2017-11-13 08:28:13 -0500

    add images

--- breaker/sketch.js
+++ breaker/sketch.js
@@ -200,7 +200,8 @@ function setup() {
   // createCanvas(w,h)
   // 'height' is variable in P5 that is set to the canvas' height
   angleMode(DEGREES)
-  createCanvas(700, 700)
+  var canvas = createCanvas(700, 700)
+  canvas.parent('canvas-area');
   start_line_length = dist(ball_start_x, ball_start_y, line_start_x, line_start_y)
 
   paddle = new Paddle(paddle_x, height-40, paddle_full_width)
